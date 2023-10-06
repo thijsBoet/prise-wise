2bda1481a28562665d5b79d2d605c32b195557b1
Matthijs Boet <m.boet@chello.nl>
2023-10-06 17:44:01 +0200

    ready for deploy

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -10,6 +10,11 @@ import {
 import { generateEmailBody, sendEmail } from '@/nodemailer';
 import { NextResponse } from 'next/server';
 
+export const maxDuration = 300; 
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
+
 export async function GET() {
 	try {
 		connectToDB();
@@ -40,7 +45,7 @@ export async function GET() {
 				};
 
 				const updatedProduct = await Product.findOneAndUpdate(
-					{ url: scrapedProduct.url },
+					{ url: product.url },
 					product,
 				);
 
